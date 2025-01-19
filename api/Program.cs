de380b855dc730a5ca3d6d1c660bf3a202168b17
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-20 00:16:51 +0300

    Stock Controller init

--- api/Program.cs
+++ api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 
@@ -23,6 +24,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
 
 
 
