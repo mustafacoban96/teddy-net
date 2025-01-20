7fac64f186f67f528ed85115da5ab4f9860045ee
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-20 15:36:32 +0300

    jwt token scheme

--- api/Program.cs
+++ api/Program.cs
@@ -2,8 +2,10 @@ using api.Data;
 using api.Interface;
 using api.Models;
 using api.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -49,6 +51,33 @@ builder.Services.AddIdentity<AppUser,IdentityRole>(options =>{
 
 
 ///////////////////////////////////////////
+
+///////jwt token scheme//////////////////
+
+builder.Services.AddAuthentication(options =>{
+    options.DefaultAuthenticateScheme = 
+    options.DefaultChallengeScheme = 
+    options.DefaultForbidScheme = 
+    options.DefaultScheme = 
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme; 
+}).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters{
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+        )
+    };
+});
+
+
+
+
+///
 
 
 
