c8666b00722c24b125bd9646764697b35415cc46
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-20 14:46:22 +0300

    pagination

--- api/Helper/QueryObject.cs
+++ api/Helper/QueryObject.cs
@@ -14,5 +14,8 @@ namespace api.Helper
 
         public bool IsDecsending { get; set; } = false;
         
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 2;
     }
 }
