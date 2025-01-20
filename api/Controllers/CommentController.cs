29780bb7d8d3810b5b88c92b2b31d4f7691226ea
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-20 12:43:39 +0300

    comment delete api

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -71,5 +71,18 @@ namespace api.Controllers
            return Ok(comment.ToCommentDto());
         }
 
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id){
+            var commentModel = await _commentRepo.DeleteAsync(id);
+
+            if (commentModel == null)
+            {
+                return NotFound("Comment does not exist");
+            }
+
+            return Ok(commentModel);
+        }
+
     }
 }
--- api/Interface/ICommentRepository.cs
+++ api/Interface/ICommentRepository.cs
@@ -11,7 +11,7 @@ namespace api.Interface
         Task<List<Comment>> GetAllAsync();
         Task<Comment?> GetByIdAsync(int id);
         Task<Comment> CreateAsync(Comment commentModel);
-
         Task<Comment?> UpdateAsync(int id, Comment commentModel);
+        Task<Comment?> DeleteAsync(int id);
     }
 }
