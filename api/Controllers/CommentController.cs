133421acfa95aa4313dc63d1c392193ed128ed53
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-20 12:31:20 +0300

    update comment in controller

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -57,5 +57,19 @@ namespace api.Controllers
             return CreatedAtAction(nameof(GetById), new {id = commentModel.Id} , commentModel.ToCommentDto());
 
         }
+
+        [HttpPut]
+        [Route("{id:int}")]
+         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
+        {
+           var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate());
+
+           if(comment == null){
+            return NotFound("Comment not found");
+           }
+
+           return Ok(comment.ToCommentDto());
+        }
+
     }
 }
