80f1dc6f8c91666485f95e811f9caf0289967d55
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-20 12:03:04 +0300

    stock exsist query in repo

--- api/Interface/IStockRepository.cs
+++ api/Interface/IStockRepository.cs
@@ -14,6 +14,8 @@ namespace api.Interface
         Task<Stock?> GetByIdAsync(int id);
         Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
         Task<Stock?> DeleteAsync(int id);
+
+        Task<bool> StockExists(int id);
         
     }
 }
